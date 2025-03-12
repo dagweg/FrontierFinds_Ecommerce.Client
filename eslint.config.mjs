8852bf353a70fececf313544f670f5d5f60e0b19
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-12 22:52:48 +0300

    add middleware for request path tracking, update Next.js config for image domains, and enhance Tailwind CSS configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,51 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintRecommended from "eslint/conf/eslint-recommended";
+import reactPlugin from "eslint-plugin-react";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Apply to all JS/TS files
+    ...eslintRecommended, // Spread eslint:recommended rules
+    plugins: {
+      react: reactPlugin,
+      prettier: prettierPlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules, // Spread prettier rules
+      "prettier/prettier": "error",
+      "react/jsx-max-props-per-line": ["error", { maximum: 1, when: "always" }],
+      "react/jsx-wrap-multilines": [
+        "error",
+        {
+          declaration: "parens-new-line",
+          assignment: "parens-new-line",
+          return: "parens-new-line",
+          arrow: "parens-new-line",
+          condition: "parens-new-line",
+          logical: "parens-new-line",
+          prop: "parens-new-line",
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      ...reactPlugin.configs.recommended.rules, // spread react recommended rules.
+    },
+  },
+  {
+    ...prettierConfig, // Disable rules that conflict with prettier.
+  },
 ];
-
-export default eslintConfig;
