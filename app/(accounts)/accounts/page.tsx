75258968e13542a9980a899047469fa2bcc896d2
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 18:48:24 +0300

    remove unused account-related pages: Accounts, VerifyAccount, and SignIn

--- app/(accounts)/accounts/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import React from "react";
-
-function AccountsPage() {
-  redirect("/accounts/signin");
-  return <div>AccountsPage</div>;
-}
-
-export default AccountsPage;
