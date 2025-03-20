30014f1e7fa68ca9a182a8824a8dd9e7002de55d
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 18:52:01 +0300

    add Settings page with title component and layout

---
+++ app/settings/page.tsx
@@ -0,0 +1,14 @@
+import Title from "@/components/custom/title";
+import React from "react";
+
+function SettingsPage() {
+  return (
+    <section className="h-screen">
+      <div className="max-w-6xl mx-auto p-4">
+        <Title text="Settings" className="text-3xl" />
+      </div>
+    </section>
+  );
+}
+
+export default SettingsPage;
