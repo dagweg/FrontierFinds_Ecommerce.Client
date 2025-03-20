c99b7398f4c69ca598ca5454542d274fcdb18611
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 18:48:38 +0300

    remove unused pre-login and account-related components

--- app/(accounts)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import Link from "next/link";
-import React from "react";
-
-function AccountsLayout({ children }: { children: React.ReactNode }) {
-  return <div className=" w-full py-10">{children}</div>;
-}
-
-export default AccountsLayout;
--- app/(accounts)/not-found.tsx
+++
@@ -1,5 +0,0 @@
-import NotFound from "@/app/not-found";
-import Link from "next/link";
-export default function NF() {
-  return <NotFound />;
-}
