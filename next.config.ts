87c18444e4c19414ccc0df9a3c85e086a8012de9
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 18:54:26 +0300

    chore: update image domains in next.config.ts for enhanced asset loading

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["imgur.com"],
+    domains: ["imgur.com", "i.imgur.com", "example.com", "res.cloudinary.com"],
   },
 };
 
