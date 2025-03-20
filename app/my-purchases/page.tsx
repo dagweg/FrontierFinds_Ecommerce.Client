363f571ef1092bf5011260df055503c68388a454
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 18:51:48 +0300

    add MyPurchases page with title component and layout

---
+++ app/my-purchases/page.tsx
@@ -0,0 +1,14 @@
+import Title from "@/components/custom/title";
+import React from "react";
+
+function MyPurchases() {
+  return (
+    <section className="h-screen">
+      <div className="max-w-6xl mx-auto p-4">
+        <Title text="My Purchases" className="text-3xl" />
+      </div>
+    </section>
+  );
+}
+
+export default MyPurchases;
