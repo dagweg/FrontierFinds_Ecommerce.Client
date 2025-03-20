3ab93467e4e3a16197482ad4e012e4e065a9dfb4
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 18:52:34 +0300

    enhance CartItemCard with quantity controls and remove functionality

--- components/custom/cart-item-card.tsx
+++ components/custom/cart-item-card.tsx
@@ -1,18 +1,20 @@
 "use client";
 import { CartItemResult } from "@/types/cart.types";
+import { Minus, Plus, Trash, Trash2 } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
 interface CartItemCardProps {
   item: CartItemResult;
+  onRemove: (itemId: string) => void;
 }
 
-const CartItemCard: React.FC<CartItemCardProps> = ({ item }) => {
+const CartItemCard: React.FC<CartItemCardProps> = ({ item, onRemove }) => {
   const { product, quantity } = item;
   const totalPriceForItem = (product.priceValueInCents * quantity) / 100; // Assuming price is in cents, converting to dollars for display
 
   return (
-    <div className="flex border rounded-lg shadow-sm p-4 mb-4">
+    <div className="flex border-b-2  shadow-sm p-4 mb-4 ">
       {/* Product Image */}
       <div className="w-24 h-24 mr-4 overflow-hidden rounded-md">
         <Image
@@ -25,28 +27,49 @@ const CartItemCard: React.FC<CartItemCardProps> = ({ item }) => {
       </div>
 
       {/* Product Details */}
-      <div className="flex flex-col justify-between flex-1">
-        <div>
-          <h3 className="text-lg font-semibold text-gray-800">
-            {product.productName}
-          </h3>
-          <p className="text-gray-600 text-sm">
-            {product.productDescription.substring(0, 100)}...
-          </p>{" "}
-          {/* Limit description length */}
-        </div>
-
-        <div className="flex items-center justify-between mt-2">
-          {/* Quantity */}
-          <div className="flex items-center">
-            <span className="text-gray-700 mr-2">Quantity:</span>
-            <span className="font-semibold">{quantity}</span>
+      <div className="flex flex-col justify-between flex-1 ">
+        <div className="flex justify-between gap-20 items-start ">
+          <div className="space-y-3">
+            <div>
+              <h3 className="text-lg font-semibold text-gray-800">
+                {product.productName}
+              </h3>
+              <p className="">
+                {` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo
+                excepturi vitae quis aperiam, impedit accusamus, facilis
+                repudiandae veritatis, commodi quia magni dignissimos debitis.
+                Asperiores obcaecati odio minus nobis saepe iusto fuga dicta
+                doloribus tempore! Aliquid corporis ducimus non facilis sapiente
+                cupiditate vel nam, quae, saepe cumque odit tenetur voluptate
+                necessitatibus.`.slice(0, 200)}
+                ...
+              </p>
+            </div>
+            {/* Quantity */}
+            <div className="flex items-center gap-3">
+              <div className="bg-blue-800 cursor-pointer text-white p-1 rounded-full">
+                <Minus size={15} />
+              </div>
+              <span className="font-semibold mx-5">{quantity}</span>
+              <div className="bg-blue-800 cursor-pointer text-white p-1 rounded-full">
+                <Plus size={15} />
+              </div>
+            </div>
+            <div>
+              <div
+                onClick={() => onRemove(item.id)}
+                className="flex items-center gap-3 text-sm text-gray-500 group/remove cursor-pointer"
+              >
+                <Trash2 size={20} className="text-neutral-500" />
+                <span className="group-hover/remove:underline font-bold text-neutral-800">
+                  Remove
+                </span>
+              </div>
+            </div>
           </div>
-
           {/* Price */}
           <div>
-            <span className="text-gray-700 mr-1">Price:</span>
-            <span className="font-semibold text-indigo-600">
+            <span className="font-bold text-neutral-600 text-lg">
               {
                 product.priceCurrency === "USD"
                   ? "$"
