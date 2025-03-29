5aefe899f3c1d62c3ba7f1deb983a8c0b861700e
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-29 12:14:26 +0300

    feat: enhance layout with new fonts and smooth scrolling; add Home page component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,37 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import {
+  Geist,
+  Geist_Mono,
+  Josefin_Sans,
+  Platypi,
+  Scope_One,
+} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/custom/nav-bar";
 import Footer from "@/components/custom/footer";
 import { BreadcrumbGenerator } from "@/components/custom/breadcrumb-generator";
 import { SessionProvider } from "@/components/providers/session-provider";
+import { Suspense } from "react";
+import { CircleDashed } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const scopeOne = Scope_One({
+  subsets: ["latin"],
+  variable: "--font-scope-one",
+  weight: "400",
+});
+
+const platypi = Platypi({
+  variable: "--font-platypi",
+  subsets: ["latin"],
+});
+
+const josefinSans = Josefin_Sans({
+  variable: "--font-josefin-sans",
   subsets: ["latin"],
 });
 
@@ -29,13 +53,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen `}
+        className={`${geistSans.variable} ${geistMono.variable} ${josefinSans.variable} ${scopeOne.variable} ${platypi.variable} antialiased flex flex-col min-h-screen font-geist bg-neutral-100`}
       >
         <SessionProvider>
-          <Navbar />
-          <BreadcrumbGenerator />
-          {children}
-          <Footer />
+          <Suspense
+            fallback={
+              <div className="min-h-full h-screen w-full flex items-center">
+                <CircleDashed size={25} className="animate-spin duration-100" />
+              </div>
+            }
+          >
+            <Navbar />
+            <BreadcrumbGenerator />
+
+            {children}
+            <Footer />
+          </Suspense>
         </SessionProvider>
       </body>
     </html>
